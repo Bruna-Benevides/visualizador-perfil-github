1918123006a48f3171560ee705f1e956cab72da6
Bruna Benevides <bruapps2015@gmail.com>
2025-12-01 11:46:22 -0300

    feat:adiciona lista com os ultimos 10 repositorios

--- src/js/githubApi.js
+++ src/js/githubApi.js
@@ -1,8 +1,17 @@
+const BASE_URL = 'https://api.github.com';
+
 export async function fetchGithubUser(userName) {
-  const BASE_URL = 'https://api.github.com';
-  const response = await fetch(`${BASE_URL}/users/${userName}`);
+    const response = await fetch(`${BASE_URL}/users/${userName}`);
   if (!response.ok) {
     throw new Error('Usuário não encontrado.');
+  }
+  return await response.json();
+}
+
+export async function fetchGithubUserRepos(userName) {
+  const response = await fetch(`${BASE_URL}/users/${userName}/repos?sort=pushed&direction=desc&per_page=10`);
+  if (!response.ok) {
+    throw new Error('Não foi possível buscar os repositórios do usuário.');
   }
   return await response.json();
 }
