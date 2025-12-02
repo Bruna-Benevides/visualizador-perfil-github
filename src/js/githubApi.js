7f5f652d5fde81914bf262cc38b537e34ed458b1
Bruna Benevides <bruapps2015@gmail.com>
2025-12-01 23:41:18 -0300

    fix: correção githubApi

--- src/js/githubApi.js
+++ src/js/githubApi.js
@@ -1,7 +1,7 @@
 const BASE_URL = 'https://api.github.com';
 
 export async function fetchGithubUser(userName) {
-    const response = await fetch(`${BASE_URL}/users/${userName}`);
+  const response = await fetch(`${BASE_URL}/users/${userName}`);
   if (!response.ok) {
     throw new Error('Usuário não encontrado.');
   }
@@ -9,9 +9,9 @@ export async function fetchGithubUser(userName) {
 }
 
 export async function fetchGithubUserRepos(userName) {
-  const response = await fetch(`${BASE_URL}/users/${userName}/repos?sort=pushed&direction=desc&per_page=10`);
+  const response = await fetch(`${BASE_URL}/users/${userName}/repos?per_page=10&sort=created`);
   if (!response.ok) {
-    throw new Error('Não foi possível buscar os repositórios do usuário.');
+    throw new Error('Repositórios não encontrados.');
   }
   return await response.json();
 }
